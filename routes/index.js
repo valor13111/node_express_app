adc4d35bacb049ff65822c33f4a83838a9fadbe6
Tyler Reed <valor13111@gmail.com>
2018-02-17 22:09:15 -0500

    Changed the default route to redirect to /catalog

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.redirect('/catalog');
 });
 
 module.exports = router;
